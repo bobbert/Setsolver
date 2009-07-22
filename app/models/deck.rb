30fa3fecb43898fe59ce7680b2fb0f4c482bdb95
Robert Phelps <bobbert@bobbert-laptop.(none)>
2009-07-22 12:24:15 -0400

    small changes to Deck and Game model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,43 +2,6 @@ class Deck < ActiveRecord::Base
   has_many :cards, :order => "facedown_position"
   belongs_to :game
 
-  # Returns an array of randomly selected Card objects, length "numcards".
-  # The Card objects returned by this method are not removed from the deck.
-  def get_random_cards( numcards = 1 )
-    dealt = []
-    return cards.to_a if numcards.to_i >= cards.length
-    id_list = cards.map {|c| c.id }
-    numcards.to_i.downto(1) do |num|
-      rnd = rand id_list.length
-      dealt << Card.find( id_list.slice!(rnd.floor) )
-    end
-    dealt
-  end
-
-  # deals "number" number of cards from facedown to faceup list.
-  # The cards are returned if successful.
-  def deal(number = 1)
-    to_be_dealt = cards.find_all do |c|
-      c.facedown_position && c.facedown_position <= number
-    end
-    num_prev_in_play = in_play.length
-    # removing from facedown to faceup list, one card per iteration
-    to_be_dealt.sort.each_with_index do |tbd_c, tbd_i|
-      tbd_c.reload
-      tbd_c.remove_from_list
-      tbd_c.faceup_position = num_prev_in_play + tbd_i + 1
-      tbd_c.save
-    end
-    save
-  end
-
-  # fills in-play game field with passed in number of cards
-  def fill_game_field(number = Game::FIELD_SIZE)
-    num_in_play = in_play.length
-    return false if num_in_play > number
-    deal (number - num_in_play)
-  end
-
   # the deck length
   def length
     cards.length
@@ -76,6 +39,7 @@ class Deck < ActiveRecord::Base
       cards << c_new
       c_new.save
     end
+    return shuffle if autoshuffle
     save
   end
 
@@ -89,6 +53,23 @@ class Deck < ActiveRecord::Base
       # (not reloading the objects is a feature here, not a bug.)
       sc.facedown_position = i + 1
       sc.save
+    end
+    save
+  end
+
+  # deals "number" number of cards from facedown to faceup list.
+  # The cards are returned if successful.
+  def deal(number = 1)
+    to_be_dealt = cards.find_all do |c|
+      c.facedown_position && c.facedown_position <= number
+    end
+    num_prev_in_play = in_play.length
+    # removing from facedown to faceup list, one card per iteration
+    to_be_dealt.sort.each_with_index do |tbd_c, tbd_i|
+      tbd_c.reload
+      tbd_c.remove_from_list
+      tbd_c.faceup_position = num_prev_in_play + tbd_i + 1
+      tbd_c.save
     end
     save
   end
