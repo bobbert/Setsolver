d77058b0beecb68639f25d8cfa2fbddde2626a83
Robert Phelps <bobbert@bobbert-laptop.(none)>
2009-08-03 12:09:44 -0400

    changed routing to :id/:action for upcoming Player controller

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:id/:action'
+  map.connect ':controller/:id/:action.:format'
 end
